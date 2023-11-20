e82d8716ae38251965342a0347a51b2234f80ba1
Rodrigo <60452545+rodrigolitzius@users.noreply.github.com>
2023-11-20 14:10:22 -0300

    changed compile script

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,4 +1,5 @@
 src_folder="./src"
-gcc $src_folder/main.c $src_folder/functions.c $src_folder/body.c -std=c17 -Wall -Wextra -pedantic -pedantic-errors -lSDL2 -lSDL2main -lm -o main
+cflags="-std=c17 -Wall -Wextra -pedantic -pedantic-errors -lSDL2 -lSDL2main -lm"
+gcc $src_folder/main.c $src_folder/functions.c $src_folder/body.c $cflags -o main
 
 ./main
