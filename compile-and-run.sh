82f90b2a8d4f9a8e9019121b416dfc786d44d19a
Rodrigo <60452545+rodrigolitzius@users.noreply.github.com>
2023-11-22 16:27:25 -0300

    Using pkg-config command

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,5 +1,9 @@
 src_folder="./src"
-cflags="-std=c17 -Wall -Wextra -pedantic -pedantic-errors -lSDL2 -lSDL2main -lm"
+
+cflags_warnings="-std=c17 -Wall -Wextra -pedantic -pedantic-errors"
+cflags_libs="$(pkg-config --cflags --libs sdl2) -lm"
+cflags="$cflags_warnings $cflags_libs"
+
 gcc $src_folder/main.c $src_folder/functions.c $src_folder/body.c $cflags -o main
 
 ./main
