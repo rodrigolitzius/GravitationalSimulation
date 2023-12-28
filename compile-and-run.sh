1094d30bee59c985954fd98364d514cb7895323d
Rodrigo <60452545+rodrigolitzius@users.noreply.github.com>
2023-12-28 06:23:16 -0300

    run script modified

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -4,6 +4,6 @@ cflags_warnings="-std=c17 -Wall -Wextra -pedantic -pedantic-errors"
 cflags_libs="$(pkg-config --cflags --libs sdl2) -lm"
 cflags="$cflags_warnings $cflags_libs"
 
-gcc $src_folder/main.c $src_folder/functions.c $src_folder/body.c $cflags -o main
+gcc $src_folder/main.c $src_folder/functions.c $src_folder/body.c $cflags $@ -o main
 
 ./main
